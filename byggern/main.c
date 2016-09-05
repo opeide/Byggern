14cd86044f3997fbd116a32bf0ff5e29c48068bf
opeide <fredrikopeide@gmail.com>
2016-09-05 19:12:10 +0200

    added print statement

--- byggern/main.c
+++ byggern/main.c
@@ -22,17 +22,17 @@ int main(void)
 	timer0_init();
 	
 	printf("*****STARTING PROGRAM*****\n");
+	
 	//Enable global interrupts
 	sei();
-	
 
-	
 	while(1)
     {		
 		printf("LED status: %d\n", status_led_get_status());		
 		timer0_execute_function_in_millis(status_led_on, 5000);
 		_delay_ms(5000);
 		printf("5 seconds passed\n");
+		printf("LED status: %d\n", status_led_get_status());
 		_delay_ms(1000);
 		status_led_off();
 		
